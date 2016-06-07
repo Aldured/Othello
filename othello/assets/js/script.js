29962fc5ec09f2dce5745c6f61bb0656a55527fa
DamienM <merle.damien@outlook.com>
2016-06-07 21:23:12 +0200

    premier control sur l'axe des x positif ok

--- othello/assets/js/script.js
+++ othello/assets/js/script.js
@@ -1,18 +1,19 @@
 var plateau =[[],[],[],[],[],[],[],[]] ;
-var verif =[[],[],[],[],[],[],[],[]] ;
 var joueur="noir";
 var autrejoueur="blanc";
 var posx;
 var posy;
-var nomb;
+var continu=true;
+var compteur=0;
+var possible=false;
 
 //charge jquery
 $(function(){
 	//génération du  plateau
 
-	for (var i = 1; i < 9; i++) {
+	for (var i = 0; i < 8; i++) {
 		$('table').append("<tr></tr>");
-		for (var y=1; y<9; y++) {
+		for (var y=0; y<8; y++) {
 			$('tr').eq(-1).append("<td></td>")
 			$('td').eq(-1).attr("id",(+i+""+y));
 		}
@@ -23,13 +24,18 @@ $(function(){
 	plateau[3][4]="noir";
 	plateau[4][3]="noir";
 
-	for (var i=1;i<9;i++){
-		for (var y=1;y<9;y++){
-			if (plateau[y-1][i-1]==="blanc"){
-				$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").addClass("blanc");
+	$("#33").off("click");
+	$("#44").off("click");
+	$("#34").off("click");
+	$("#43").off("click");
+
+	for (var i=0;i<8;i++){
+		for (var y=0;y<8;y++){
+			if (plateau[y][i]==="blanc"){
+				$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").removeClass().addClass("blanc");
 			}
-			if (plateau[y-1][i-1]==="noir") {
-				$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").addClass("noir");
+			if (plateau[y][i]==="noir") {
+				$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").removeClass().addClass("noir");
 			}
 
 		}
@@ -38,71 +44,68 @@ $(function(){
 
 
 	$("td").click(function(){
-		// désactivation de la case jouée
-		$(this).off("click");
+		// verification si case deja occupée
 
-		// enregistrement des coordonées de la case jouées
-		posx= $(this).index();
-		posy= $(this).parent().index();
-
-		// écriture de la case joué dans le tableaux en fonction du joueur, et changement de joueur
-		if (joueur ==="noir") {
-			plateau[posy][posx]='noir';
-			nomb=cherche();
-			joueur="blanc";
-			autrejoueur="noir";
-
+		if (($(this).hasClass("blanc"))||($(this).hasClass("noir"))){
+			alert("cette case est occupée !!!")
 		}
 		else {
-			plateau[posy][posx]='blanc';
-			nomb=cherche();
-			joueur="noir";
-			autrejoueur="blanc";
-		}
 
+			// enregistrement des coordonées de la case jouées
+			posx= $(this).index();
+			posy= $(this).parent().index();
 
-
-
-
-
-
-
-
-		for (var i=1;i<9;i++){
-			for (var y=1;y<9;y++){
-				if (plateau[y-1][i-1]==="blanc"){
-					$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").addClass("blanc");
-				}
-				if (plateau[y-1][i-1]==="noir") {
-					$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").addClass("noir");
-				}
+			// écriture de la case joué dans le tableaux en fonction du joueur, et changement de joueur
+			if (joueur ==="noir") {
+				plateau[posy][posx]='noir';
 
 			}
+			else {
+				plateau[posy][posx]='blanc';
+
+			}
+
+			//test puis retournement
+			possible=false;
+			continu=true;
+			compteur=0;
+
+			for (i=posx;continu;i++){
+				if(plateau[posy][i+1]===autrejoueur){
+					compteur++;
+				}
+				else {
+					continu=false;
+					if (plateau[posy][i+1]===joueur){
+					possible=true;
+					}
+				}
+			}
+
+
+			//changement de joueur
+			if (joueur==="noir") {
+				joueur="blanc";
+				autrejoueur="noir";
+			}
+			else{
+				joueur="noir";
+				autrejoueur="blanc";
+			}
+
+			for (var i=0;i<8;i++){
+				for (var y=0;y<8;y++){
+					if (plateau[y][i]==="blanc"){
+						$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").removeClass().addClass("blanc");
+					}
+					if (plateau[y][i]==="noir") {
+						$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").removeClass().addClass("noir");
+					}
+				}
+			}
+
 		}
-
 	}); //fin de fonction click
-
-
-
 
 });//fin jquery
 
-//recherche un piont de meme couleur sur la ligne
-function cherche (){
-	var continu=true;
-	var compteur=0;
-	for (i=1;continu=false;i++){
-		if(plateau[posy][posx+i]===autrejoueur){
-			continu=true;
-			compteur++;
-
-		}
-		else {
-			continu=false;
-			if (plateau[posy][posx+i]===joueur){
-			return compteur;
-			}
-		}
-	}
-
-};
