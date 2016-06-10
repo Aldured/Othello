595c00b4488c8e8c118f71a198d94d0f0ac6da68
DamienM <merle.damien@outlook.com>
2016-06-10 16:30:57 +0200

    ça fonctionne, avec toutes les régles du jeu, reste encore un peu de mise en page, et affichage des scores et autres agréements

--- othello/assets/js/script.js
+++ othello/assets/js/script.js
@@ -3,10 +3,10 @@ var joueur="noir";
 var autrejoueur="blanc";
 var posx;
 var posy;
-var continu=true;
-var compteur=0;
-var possible=false;
-
+var test=false;
+var jeupossible;
+var nbnoir=0;
+var nbblanc=0;
 //charge jquery
 $(function(){
 	//génération du  plateau
@@ -41,68 +41,141 @@ $(function(){
 		// verification si case deja occupée
 
 		if (($(this).hasClass("blanc"))||($(this).hasClass("noir"))){
-			alert("cette case est occupée !!!")
+			alert("cette case est occupée !!!");
 		}
 		else {
 
-			// enregistrement des coordonées de la case jouées
-			posx= $(this).index();
-			posy= $(this).parent().index();
-
-			// écriture de la case joué dans le tableaux en fonction du joueur, et changement de joueur
-			if (joueur ==="noir") {
-				plateau[posy][posx]='noir';
-
-			}
-			else {
-				plateau[posy][posx]='blanc';
-
-			}
-
-			//test puis retournement
-			//test y0 x+
-			cherche("","+");
-			//test y0 x-
-			cherche("","-");
-			//test y+ x0
-			cherche("+","");
-			//test y- x0
-			cherche("-","");
-			//test y+ x+
-			cherche("+","+");
-			//test y+ x-
-			cherche("+","-");
-			//test y- x+
-			cherche("-","+");
-			//test y- x-
-			cherche("-","-");
-
-
-			//changement de joueur
-			if (joueur==="noir") {
-				joueur="blanc";
-				autrejoueur="noir";
-			}
-			else{
-				joueur="noir";
-				autrejoueur="blanc";
-			}
-
-			for (var i=0;i<8;i++){
-				for (var y=0;y<8;y++){
-					if (plateau[y][i]==="blanc"){
-						$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").removeClass().addClass("blanc");
-					}
-					if (plateau[y][i]==="noir") {
-						$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").removeClass().addClass("noir");
+			//test si solution possible
+			jeupossible=false;
+			for (i=0 ;i<8;i++ ){
+				for (j=0; j<8;j++){
+					posx=i;
+					posy=j;
+					if ((plateau[j][i]!="noir")&&(plateau[j][i]!="blanc")&&((testdebut("","+"))||(testdebut("","-"))||(testdebut("+",""))||(testdebut("-",""))||(testdebut("+","+"))||(testdebut("+","-"))||(testdebut("-","+"))||(testdebut("-","-")))){
+						jeupossible=true;
 					}
 				}
 			}
 
+			if(jeupossible){
+				// enregistrement des coordonées de la case jouées
+				posx= $(this).index();
+				posy= $(this).parent().index();
+
+
+
+				//controle combinaison possible, obligatoir pour jouer
+				if ((testdebut("","+"))||(testdebut("","-"))||(testdebut("+",""))||(testdebut("-",""))||(testdebut("+","+"))||(testdebut("+","-"))||(testdebut("-","+"))||(testdebut("-","-"))){
+
+					// écriture de la case joué dans le tableaux en fonction du joueur, et changement de joueur
+					if (joueur ==="noir") {
+						plateau[posy][posx]='noir';
+
+					}
+					else {
+						plateau[posy][posx]='blanc';
+
+					}
+
+
+					//test puis retournement
+					//test y0 x+
+					cherche("","+");
+					//test y0 x-
+					cherche("","-");
+					//test y+ x0
+					cherche("+","");
+					//test y- x0
+					cherche("-","");
+					//test y+ x+
+					cherche("+","+");
+					//test y+ x-
+					cherche("+","-");
+					//test y- x+
+					cherche("-","+");
+					//test y- x-
+					cherche("-","-");
+
+
+					//changement de joueur
+					if (joueur==="noir") {
+						joueur="blanc";
+						autrejoueur="noir";
+					}
+					else{
+						joueur="noir";
+						autrejoueur="blanc";
+					}
+
+					for (var i=0;i<8;i++){
+						for (var y=0;y<8;y++){
+							if (plateau[y][i]==="blanc"){
+								$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").removeClass().addClass("blanc");
+							}
+							if (plateau[y][i]==="noir") {
+								$("#"+y+i).html("<i class=\"fa fa-circle\" aria-hidden=\"true\"></i>").removeClass().addClass("noir");
+							}
+						}
+					}
+				}
+				else {alert("vous devez jouer une case avec combinaison possible");}
+			}
+			else{
+				alert("pas de position possible pour joueur "+joueur+",il doit passer son tour.");
+				//changement de joueur
+				if (joueur==="noir") {
+					joueur="blanc";
+					autrejoueur="noir";
+				}
+				else{
+					joueur="noir";
+					autrejoueur="blanc";
+				}
+			}
+			if(joueur==="noir"){ $("#joueur").removeClass().addClass("noir");	}
+			else{$("#joueur").removeClass().addClass("blanc");}
 		}
 	}); //fin de fonction click
 
 });//fin jquery
+
+function testdebut (sensy,sensx){
+
+			var possible=false;
+			var continu=true;
+			var compteur=0;
+			var x=posx;
+			var y=posy;
+			var limity=false;
+			var mouvy;
+			var mouvx;
+
+			if (sensy==="+"){if(y===7){limity=true;}}
+			if (sensy==="-"){if(y===0){limity=true;}}
+
+			while (continu && (!limity)){
+
+				if (sensy==="+"){mouvy=y+1;}
+				if (sensy==="-"){mouvy=y-1;}
+				if (sensy===""){mouvy=y;}
+				if (sensx==="+"){mouvx=x+1;}
+				if (sensx==="-"){mouvx=x-1;}
+				if (sensx===""){mouvx=x;}
+
+				if(plateau[mouvy][mouvx]===autrejoueur){compteur++;}
+				else{
+					continu=false;
+					if ((plateau[mouvy][mouvx]===joueur)&(compteur>0)){possible=true;}
+				}
+				if (sensy==="+"){y++;}
+				if (sensy==="-"){y--;}
+				if (sensx==="+"){x++;}
+				if (sensx==="-"){x--;}
+				if (sensy==="+"){if(y===7){limity=true;}}
+				if (sensy==="-"){if(y===0){limity=true;}}
+			}
+			return possible;
+		}
 
 function cherche (sensy,sensx){
 
@@ -161,3 +234,6 @@ function cherche (sensy,sensx){
 		}
 	}
 }
+
+
+
